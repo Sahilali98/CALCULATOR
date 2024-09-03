0862a27711c57b16d4d777893a5ce9f5ccfc9e96
SK SAHIL ALI <sahilali.ind@gmail.com>
2024-09-03 23:11:22 +0530

    add scientific function

--- SIMPLE CALCULATOR  2/test.js
+++ SIMPLE CALCULATOR  2/test.js
@@ -1,2 +1,24 @@
-let math = Math.tan(0);
-console.log(math)
+let math = Math.tan(30);
+
+
+// console.log(math);
+
+// console.log(Math.PI)
+
+// console.log((radian*180)/Math.PI);
+let deg = (math*180)/Math.PI;
+// console.log(deg/180)
+
+// console.log(Math.sqrt(25))
+
+let list = [10, 20, 30, 50]
+
+list.splice(0,3,40);
+// console.log(list)
+
+console.log(Math.E)
+
+console.log(Math.LN2)
+console.log(Math.log(30))
+
+console.log(Math.pow(4,2))
